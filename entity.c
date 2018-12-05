5a4441e6773f8f902a92ffd2f03226da901cf39f
Caskey, Damon V <dc@caskeys.com>
2018-12-05 12:59:26 -0500

    Access and mutate for entity.

---
+++ entity.c
@@ -0,0 +1,41 @@
+#include "data/scripts/dc_terrain/config.h"
+
+#import "data/scripts/dc_terrain/instance.c"
+
+void dc_terrain_get_entity()
+{
+	char id;
+	void result;
+
+	// Concatenate instance and key to create an id.
+	id = dc_terrain_get_instance() + DC_TERRAIN_VAR_KEY_ENT;
+
+	// Get value from local var.
+	result = getlocavar(id);
+
+	// If result is empty or invalid, use default.
+	if (typeof(result) != openborconstant("VT_INTEGER"))
+	{
+		result = DC_TERRAIN_DEFAULT_ENT;
+	}
+
+	return result;
+}
+
+void dc_terrain_set_entity(int value)
+{
+	char id;
+
+	// Concatenate instance and key to create an id.
+	id = dc_terrain_get_instance() + DC_TERRAIN_VAR_KEY_ENT;
+
+	// If value is same as default, then make
+	// sure the local var is deleted. Otherwise
+	// store the value.
+	if (value == DC_TERRAIN_DEFAULT_ENT)
+	{
+		value = NULL();
+	}
+	
+	setlocalvar(id, value);
+}
